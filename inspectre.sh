d38b667649dfb5e16bfaf3ef1766ee65a4b756c7
thefunkygibbon <github@thefunkygibbon.net>
2026-04-09 18:14:17 +0100

    Update inspectre.sh

--- inspectre.sh
+++ inspectre.sh
@@ -5,11 +5,11 @@ set -e
 git fetch origin
 git reset --hard origin/main
 git clean -fd
-
+chmod 777 inspectre.sh
 docker compose down --remove-orphans
 docker image rm inspectre-probe inspectre-web 2>/dev/null || true
 docker builder prune -af
 docker compose build --no-cache --pull
 docker compose up --force-recreate
 docker compose down --remove-orphans
-chmod 777 inspectre.sh
+
